f4ec0580c5a01984e472c27ee1b72f32f3ac815b
Kiri100zz <kirita11024@gmail.com>
2026-08-19 11:03:57 +0700

    initial commit: add greeting.js

---
+++ greeting.js
@@ -0,0 +1,14 @@
+// Week 1 Git Playground
+// You will edit this file several times during the lab and commit each change.
+// Follow exercises.md step by step.
+let user1 = "Ketya";
+let user2 = "Robin";
+let user3 = "Heng"
+let user4 = "Kiri";
+
+console.log("Hello, Git!");
+console.log(`Hello Kiri`);
+console.log(`${user1} always kiss ${user3}`);
+console.log(`${user3} alwasy ask ${user2}`);
+console.log(`${user4} always the best`);
+
